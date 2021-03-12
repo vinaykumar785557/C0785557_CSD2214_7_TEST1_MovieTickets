acc10f515b1c5a09c2d27fda4d711ab47b6eb463
vinaykumar785557 <77814463+vinaykumar785557@users.noreply.github.com>
2021-03-11 21:14:36 -0500

    validating checkbox seats

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 var selectBox = form.fieldname;
 var selectedValue = selectBox.options[selectBox.selectedIndex].value
 var selectedText = selectBox.options[selectBox.selectedIndex].text
+
+function checkForm(form)
+  {
+    if(form.checkboxfield.checked) {
+      alert("You selected seat");
+    } else {
+      alert("The seat that you are looking is already reserved");
+    }
+    return false;
+  }
