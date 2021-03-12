1537d2004ee73f20a631bfc1eb5ce00b30383e6d
vinaykumar785557 <77814463+vinaykumar785557@users.noreply.github.com>
2021-03-11 20:56:50 -0500

    linked html with paymentform

--- index.js
+++
@@ -1,4 +0,0 @@
-const container = document.querySelector('.seat booking');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-const count = document.getElementById('count');
-const price = document.getElementById('price');
