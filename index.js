bc9bdf588c0a6be0e46538dd6b6a9c2b949c1ea5
vinaykumar785557 <77814463+vinaykumar785557@users.noreply.github.com>
2021-03-11 20:34:55 -0500

    added span element to index page

---
+++ index.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('.seat booking');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const price = document.getElementById('price');
