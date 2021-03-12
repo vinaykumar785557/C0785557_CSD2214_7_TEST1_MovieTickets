c88f774970cbe0c866e0c6e9fe0a23cf031eb457
vinaykumar785557 <77814463+vinaykumar785557@users.noreply.github.com>
2021-03-11 19:54:22 -0500

    started writng function for validation

--- paymentform.js
+++ paymentform.js
@@ -1,1 +1,5 @@
-var cardHolder=
+function validatepayment(){
+    var cardholder=document.getElementById("owner")
+    var cardnumber=document.getElementById("cardNumber")
+    var cvv=document.getElementById("cvv")
+}
